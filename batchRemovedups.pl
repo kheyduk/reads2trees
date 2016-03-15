e3f063a7b4bce0a6000a971aaffd83cedee6a4a3
Karolina Heyduk <heyduk@uga.edu>
2016-03-15 12:17:02 -0400

    Updating script
    Changing how fasta sequence names were parsed

--- batchRemovedups.pl
+++ batchRemovedups.pl
@@ -3,8 +3,6 @@ use strict;
 use warnings;
 use Bio::SeqIO;
 
-#Karolina Heyduk - heyduk@uga.edu  -2014
-
 my @infiles = glob("*targets.fasta");
 foreach my $infile (@infiles) {
 my %sequences;
@@ -12,14 +10,12 @@ my @dups;
 my $io_obj = Bio::SeqIO->new(-file => "$infile", -format => "fasta" );
 while (my $seq_obj = $io_obj->next_seq) {
 	my $id = $seq_obj->id;
-	my ($contig, $ID, $ID2, $exon) = split(/\_/, $id);
-	my $orthoID = "$ID\_$exon";
 	my $seq = $seq_obj->seq;    
-    if (defined ($sequences{$orthoID})) {
-        push (@dups, $orthoID);      
+    if (defined ($sequences{$id})) {
+        push (@dups, $id);      
         }
        else {
-        $sequences{$orthoID} = $seq;
+        $sequences{$id} = $seq;
 		}
 	}
 
@@ -29,14 +25,12 @@ my $io_obj1 = Bio::SeqIO->new(-file => "$infile", -format => "fasta" );
 #my $counter = 1;
 while (my $seq_obj1 = $io_obj1->next_seq) {
         my $id1 = $seq_obj1->id;
-	my ($contig, $ID, $ID2, $exon) = split(/\_/, $id1);
-	my $orthoID = "$ID\_$exon";
         my $seq1 = $seq_obj1->seq;
-        if ($orthoID ~~ @dups) {
-                print OUT2 ">$orthoID\n$seq1\n";
+        if ($id1 ~~ @dups) {
+                print OUT2 ">$id1\n$seq1\n";
         }
         else {
-                print OUT ">$orthoID\n$seq1\n";
+                print OUT ">$id1\n$seq1\n";
         }
 }       
 close OUT;
